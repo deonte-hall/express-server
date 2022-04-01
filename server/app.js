2a27b2616d0aabda0429c4499f8b881ec2474548
Deonte H (Sasko) <deonte@news-MacBook-Pro-2.local>
2022-04-01 15:43:01 -0700

    Finished my express servergit add . whoop

--- server/app.js
+++ server/app.js
@@ -1,10 +1,30 @@
 // import files and packages up here
 
-
+const  express = require('express');
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const data = require('./data.json');
 // create your express server below
-var app;
 
-// add your routes and middleware below
+let app = express();
+
+app.use(bodyParser.json());
+app.use(morgan('dev'));
+
+// route handler
+
+app.get('/', ( req, res) => {
+    res.status(200).send('Loaded. Site is functional!')
+});
+
+  app.get('/data', (req, res) => {
+
+     res.status(200).send(data);
+
+  });
+
+// listen for incoming connections
 
 // finally export the express application
+
 module.exports = app;
